6f9ddd576070f875eb4abc5d68c595800523fc26
Clément <c.lartot@student.isartdigital.com>
2017-10-02 16:45:16 +0200

    Add function LoadFile(std::string, int) -> std::vector<std::vector<unsigned int>>

--- VisualProjects/NeuralNetworkHandDigits/DataLoader.cpp
+++ VisualProjects/NeuralNetworkHandDigits/DataLoader.cpp
@@ -1,4 +1,62 @@
+#include <iostream>
+#include <fstream>
+
 #include "DataLoader.h"
+
+std::vector<std::vector<unsigned int>> DataLoader::LoadFile(std::string const& relativeFilePath, int numberOfImages)
+{
+	std::cout << "Open file : " << relativeFilePath << std::endl;
+
+	std::ifstream file(relativeFilePath, std::ios::binary);
+
+	if (file.is_open())
+	{
+		int magicNumber = 0;
+		file.read((char*)&magicNumber, 4);
+		magicNumber = ConvertToInt(magicNumber);
+
+		int nbImagesInFile = 0;
+		file.read((char*)&nbImagesInFile, 4);
+		nbImagesInFile = ConvertToInt(nbImagesInFile);
+
+		if (nbImagesInFile < numberOfImages)
+		{
+			std::cout << "Not enough images in file" << std::endl;
+			return std::vector<std::vector<unsigned int>>();
+		}
+
+		int nbRows = 0;
+		file.read((char*)&nbRows, 4);
+		nbRows = ConvertToInt(nbRows);
+
+		int nbColumns = 0;
+		file.read((char*)&nbColumns, 4);
+		nbColumns = ConvertToInt(nbColumns);
+
+		int nbPixels = nbRows * nbColumns;
+
+		std::vector<std::vector<unsigned int>> data;
+		data.resize(numberOfImages, std::vector<unsigned int>(nbPixels));
+
+		for (int imageIdx = 0; imageIdx < numberOfImages; ++imageIdx)
+		{
+			for (int rowIdx = 0; rowIdx < nbRows; ++rowIdx)
+			{
+				for (int columnIdx = 0; columnIdx < nbColumns; ++columnIdx)
+				{
+					unsigned char pixelColor = 0;
+					file.read((char*)&pixelColor, 1);
+					data[imageIdx][(rowIdx * nbRows) + columnIdx] = (unsigned int)pixelColor;
+				}
+			}
+		}
+
+		return data;
+	}
+
+	std::cout << "Can't open file" << std::endl;
+	return std::vector<std::vector<unsigned int>>();
+}
 
 int DataLoader::ConvertToInt(int value)
 {
--- VisualProjects/NeuralNetworkHandDigits/DataLoader.h
+++ VisualProjects/NeuralNetworkHandDigits/DataLoader.h
@@ -1,10 +1,15 @@
 #pragma once
 
+#include <vector>
+#include <string>
+
 class DataLoader
 {
 public:
 	DataLoader() = default;
 	~DataLoader() = default;
+
+	std::vector<std::vector<unsigned int>> LoadFile(std::string const& relativeFilePath, int numberOfImages);
 
 private:
 	int ConvertToInt(int value);
