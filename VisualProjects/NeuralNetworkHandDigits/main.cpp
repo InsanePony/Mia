3894322d78c816798d8d6c95b3dabe485239aace
Clément <c.lartot@student.isartdigital.com>
2017-10-02 16:47:11 +0200

    Test function LoadFile in main

--- VisualProjects/NeuralNetworkHandDigits/main.cpp
+++ VisualProjects/NeuralNetworkHandDigits/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
+#include "DataLoader.h"
+
 int main()
 {
 	std::cout << "Program start" << std::endl;
+
+	DataLoader* loader = new DataLoader();
+
+	loader->LoadFile("../../MNIST data/train-data-pixels-value", 1000);
 
 	std::cin.ignore();
 	return 0;
