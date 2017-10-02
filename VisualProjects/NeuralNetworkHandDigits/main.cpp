d0a9e01d33db754658d4ddca56aa423c3ca0cac4
Clément <c.lartot@student.isartdigital.com>
2017-10-02 16:57:04 +0200

    Add a small portion of code to "visualize" images (in comment)

--- VisualProjects/NeuralNetworkHandDigits/main.cpp
+++ VisualProjects/NeuralNetworkHandDigits/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <windows.h>
 
 #include "DataLoader.h"
 
@@ -8,7 +9,15 @@ int main()
 
 	DataLoader* loader = new DataLoader();
 
-	loader->LoadFile("../../MNIST data/train-data-pixels-value", 1000);
+	std::vector<std::vector<unsigned int>> data = loader->LoadFile("../../MNIST data/train-data-pixels-value", 1000);
+
+	/*for (int idx = 0; idx < 1000; ++idx)
+	{
+		loader->PrintImage(data[idx]);
+
+		Sleep(1000);
+		system("cls");
+	}*/
 
 	std::cin.ignore();
 	return 0;
