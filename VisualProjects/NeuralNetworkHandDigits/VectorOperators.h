cc4599316cb060712a7acb3776a91865fe1d3c04
Clément <c.lartot@student.isartdigital.com>
2017-10-16 15:21:13 +0200

    Add operator std::vector * (std::vector, typename)

--- VisualProjects/NeuralNetworkHandDigits/VectorOperators.h
+++ VisualProjects/NeuralNetworkHandDigits/VectorOperators.h
@@ -8,7 +8,7 @@ std::vector<T> operator - (std::vector<T> const& a, std::vector<T> const& b)
 {
 	if (a.size() != b.size())
 	{
-		std::cout << "Error in std::vector<T> operator - (std::vector<float> const& a, std::vector<unsigned int> const& b) : different size" << std::endl;
+		std::cout << "Error in std::vector<T> operator - (std::vector<T> const& a, std::vector<T> const& b) : different size" << std::endl;
 		abort();
 	}
 
@@ -19,6 +19,20 @@ std::vector<T> operator - (std::vector<T> const& a, std::vector<T> const& b)
 
 	for (size_t idx = 0; idx < size; ++idx)
 		result[idx] = a[idx] - b[idx];
+
+	return result;
+}
+
+template <typename T>
+std::vector<T> operator * (std::vector<T> const& vector, T value)
+{
+	std::vector<T> result;
+
+	size_t size = vector.size();
+	result.resize(size);
+
+	for (size_t idx = 0; idx < size; ++idx)
+		result[idx] = vector[idx] * value;
 
 	return result;
 }
