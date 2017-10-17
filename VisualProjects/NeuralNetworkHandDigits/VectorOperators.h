3296cd9563cdf424bfcfe190dc1128c8c4fc33fa
Clément <c.lartot@student.isartdigital.com>
2017-10-17 13:03:59 +0200

    Add struct Utility with static function Transpose(std::vector, unsigned int, unsigned int) -> std::vector

--- VisualProjects/NeuralNetworkHandDigits/VectorOperators.h
+++ VisualProjects/NeuralNetworkHandDigits/VectorOperators.h
@@ -3,6 +3,33 @@
 #include <vector>
 #include <iostream>
 
+struct Utility
+{
+	template <typename T>
+	static std::vector<T> Transpose(std::vector<T> const& vector, unsigned int nbRow, unsigned int nbColumn)
+	{
+		unsigned int totalSize = nbRow * nbColumn;
+		if (totalSize != vector.size())
+		{
+			std::cout << "Error in Transpose : given row or column is incorrect" << std::endl;
+			abort();
+		}
+
+		std::vector<T> result;
+		result.resize(totalSize);
+
+		for (unsigned int idx = 0; idx < totalSize; ++idx)
+		{
+			int columnIdx = idx / nbRow;
+			int rowIdx = idx % nbRow;
+
+			result[idx] = vector[rowIdx * nbColumn + columnIdx];
+		}
+
+		return result;
+	}
+};
+
 template <typename T>
 std::vector<T> operator - (std::vector<T> const& a, std::vector<T> const& b)
 {
