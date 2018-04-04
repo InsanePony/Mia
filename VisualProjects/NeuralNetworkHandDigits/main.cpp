eea7778a28a012ccd2ff09702cf14245f767481b
Clément <c.lartot@student.isartdigital.com>
2018-04-04 10:15:39 +0200

    Swap evaluation data and test data

--- VisualProjects/NeuralNetworkHandDigits/main.cpp
+++ VisualProjects/NeuralNetworkHandDigits/main.cpp
@@ -13,9 +13,9 @@ int main()
 
 	std::vector<std::array<std::vector<double>, 2>> data = loader->LoadData("../../MNIST data/train-data-pixels-value", "../../MNIST data/train-data-numbers", 60000);
 	std::vector<std::array<std::vector<double>, 2>> trainData(data.begin(), data.begin() + 50000);
-	std::vector<std::array<std::vector<double>, 2>> testData(data.begin() + 50000, data.end());
+	std::vector<std::array<std::vector<double>, 2>> evaluationData(data.begin() + 50000, data.end());
 
-	std::vector<std::array<std::vector<double>, 2>> evaluationData = loader->LoadData("../../MNIST data/test-data-pixels-value", "../../MNIST data/test-data-numbers", 10000);
+	std::vector<std::array<std::vector<double>, 2>> testData = loader->LoadData("../../MNIST data/test-data-pixels-value", "../../MNIST data/test-data-numbers", 10000);
 
 	/*for (int idx = 0; idx < 1000; ++idx)
 	{
@@ -34,7 +34,7 @@ int main()
 	}*/
 
 	Network* net = new Network({ 784, 30, 10 });
-	net->StartLearning(trainData, 30, 1.0, 10, testData);
+	net->StartLearning(trainData, 30, 3.0, 10, testData);
 
 	std::cin.ignore();
 	return 0;
